ba71b8b133ca0a19eaad9fc46695d6fbda6c8658
Ken <120130815+Moucchi@users.noreply.github.com>
2023-04-04 02:02:37 +0200

    Finalisation sprint 4

--- warBuilder.bat
+++ warBuilder.bat
@@ -1,3 +1,7 @@
+xcopy /Y .\index.jsp .\Test_framework
+xcopy /Y .\url.jsp .\Test_framework
 jar cvf deployed.war .\Test_framework
 xcopy /Y .\deployed.war D:\Tools\Tomcat\webapps\
 del .\deployed.war
+del .\index.jsp
+del .\url.jsp
