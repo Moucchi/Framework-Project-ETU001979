6373ed07bc1264fbb48d4476fc92926fb1850959
Ken <120130815+Moucchi@users.noreply.github.com>
2023-04-26 22:04:33 +0300

    Finalisation_v_01

--- jarBuilder.bat
+++ jarBuilder.bat
@@ -1,5 +1,6 @@
-jar cvf framework.jar .\framework
-xcopy /Y framework.jar .\Test_framework\lib\
+cd .\Framework\Build
+jar cvf framework.jar *
+xcopy /Y framework.jar D:\Projects\Git\Framework-Project-ETU001979\Test_framework\lib\
 xcopy /Y framework.jar D:\Tools\lib\
 xcopy /Y framework.jar D:\Tools\Tomcat\lib\
 del framework.jar
--- warBuilder.bat
+++ warBuilder.bat
@@ -1,3 +1,6 @@
-jar cvf deployed.war .\Test_framework
+cd D:\Projects\Git\Framework-Project-ETU001979\Test_framework\WEB-INF\classes
+javac -d . *.java
+cd D:\Projects\Git\Framework-Project-ETU001979\Test_framework\
+jar cvf deployed.war *
 xcopy /Y .\deployed.war D:\Tools\Tomcat\webapps\
 del .\deployed.war
