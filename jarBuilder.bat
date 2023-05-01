dbffb46e17a6cbe3eecbf2b504cb03d50c273a5a
Ken <120130815+Moucchi@users.noreply.github.com>
2023-05-01 12:40:24 +0300

    ReorganisationFinale

--- jarBuilder.bat
+++ jarBuilder.bat
@@ -1,6 +1,5 @@
-cd .\Framework\Build
+cd D:\Projects\Git\Framework-Project-ETU001979\Framework\Build
 jar cvf framework.jar *
 xcopy /Y framework.jar D:\Projects\Git\Framework-Project-ETU001979\Test_framework\lib\
 xcopy /Y framework.jar D:\Tools\lib\
 xcopy /Y framework.jar D:\Tools\Tomcat\lib\
-del framework.jar
